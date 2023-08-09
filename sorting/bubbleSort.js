e14fbf30f91cb5de0705a2469dc75033d6130e74
Bharath <kumarbharath63@gmail.com>
2023-08-09 22:54:17 +0530

    added another method

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,4 +1,4 @@
-function bubbleSort(array){
+/*function bubbleSort(array){
     for(i = array.length-1; i > 0; i--){
         for(let j=0; j < i ; j++){
             if(array[j] > array[j+1]){
@@ -9,20 +9,22 @@ function bubbleSort(array){
         }
     }
     return array
-}
-
-/*function bubbleSort(array){
-    let swapped;
-    do{
-        swapped = false
-        for(let i=0; i < array.length-1; i++){
-            let temp = array[i];
-            array[i] = array[i + 1];
-            array[i + 1] = temp;
-            swapped =  true
-        }
-    }while(swapped)
-    return array
 }*/
 
-console.log(bubbleSort([-1,-5,3,2,4]))
+function bubbleSort(array) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        let temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return array;
+}
+
+console.log(bubbleSort([-1, -5, 3, 2, 4]));
